6d675bb3f192b598e28de6aadadc3760640e066b
mingliu0608 <1933986099@qq.com>
2025-01-02 13:00:11 +0800

    init commit

---
+++ src/main/java/com/hmdp/service/IUserService.java
@@ -0,0 +1,16 @@
+package com.hmdp.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.entity.User;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author mingliu
+ * @since 2021-12-22
+ */
+public interface IUserService {
+
+}
